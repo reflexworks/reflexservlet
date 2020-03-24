96c5e09d78f9e752f67e6cc264fd8ea7e02a6226
terada <terada@192.168.0.14>
2020-03-24 15:00:02 +0900

    smtpユーザ名とfromが異なる場合のテスト

--- src/test/java/jp/reflexworks/test/MailSample.java
+++ src/test/java/jp/reflexworks/test/MailSample.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.Date;
 
-import javax.mail.Session;
+import javax.mail.Authenticator;
 import javax.mail.Message;
-import javax.mail.Transport;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.InternetAddress;
@@ -24,21 +26,20 @@ public class MailSample {
 	 * @param args
 	 * to@example.com from@example.net password smtp.gmail.com 587 true
 	 * <ol>
-	 * <li>宛先(to)</li>
-	 * <li>送信元(from)</li>
-	 * <li>送信元パスワード</li>
-	 * <li>smtpサーバ</li>
-	 * <li>ポート番号</li>
-	 * <li>メール送信プロトコル "smtp"か"smtps"を指定する。</li>
-	 * <li>STARTTLSを有効にする場合true、有効にしない場合false</li>
-	 * <li>(テスト用)デバッグ機能をONにする場合true, OFFにする場合false</li>
+	 * <li>[0]宛先(to)</li>
+	 * <li>[1]送信元(from)</li>
+	 * <li>[2]送信元パスワード</li>
+	 * <li>[3]smtpサーバ</li>
+	 * <li>[4]ポート番号</li>
+	 * <li>[5](テスト用)デバッグ機能をONにする場合true, OFFにする場合false</li>
+	 * <li>[6]SMTP認証ユーザ</li>
 	 * </ol>
 	 */
 	public static void main(String[] args) {
 
 		System.out.println("MailSample start.");
-		
-		if (args.length != 6) {
+
+		if (args.length < 6) {
 			usage();
 			System.exit(1);
 		}
@@ -50,13 +51,17 @@ public class MailSample {
 		String password = args[2];
 		String host = args[3];
 		String port = args[4];
-		
+
 		// TODO test
 		//String smtpUser = from;
 		//from = "=?iso-2022-jp?B?SVRwcm8bJEIlYSE8JWsbKEI=?= <" + from + ">";
 		// msg.setFrom(new InternetAddress(msg_from,"お名前","iso-2022-jp"));
-		
+
 		boolean debug = Boolean.valueOf(args[5]).booleanValue();
+		String authUser = null;
+		if (args.length > 6) {
+			authUser = args[6];
+		}
 
         Transport transport = null;
 		try {
@@ -70,19 +75,28 @@ public class MailSample {
 			props.put("mail.smtp.port", port);  // サブミッションポート
 			props.put("mail.smtp.auth", "true");   // SMTP 認証を行う
 			props.put("mail.smtp.starttls.enable", "true");   // STARTTLS
-			
+
 			// TODO test
 			//props.put("mail.smtp.user", smtpUser);
-			
+
 			if (debug) {
 				props.put("mail.debug", args[3]);
 			}
 
-			Session session = Session.getInstance(props);
-			//SimpleAuthenticator sa = new SimpleAuthenticator(from, password);
-			//Session session = Session.getInstance(props, sa);
+			Session session = null;
+			//if (authUser != null) {
+			//	Authenticator sa = new SMTPAuthenticator(authUser, password);
+			//	session = Session.getDefaultInstance(props, sa);
+			//} else {
+				session = Session.getInstance(props);
+			//}
+
+			if (authUser == null) {
+				authUser = from;
+			}
+
 			session.setDebug(debug);
-			
+
 			MimeMessage msg = new MimeMessage(session);
 			//msg.setFrom(new InternetAddress(from));
 			msg.setFrom(iaFrom);
@@ -106,11 +120,12 @@ public class MailSample {
 			msg.setHeader("Content-Transfer-Encoding", "7bit");
 
 			//Transport.send(msg);
-            //transport = session.getTransport("smtp");
-            transport = session.getTransport("smtps");
-            transport.connect(from, password);
-            transport.sendMessage(msg, msg.getAllRecipients());
-		
+			//transport = session.getTransport("smtp");
+			transport = session.getTransport("smtps");
+			//transport.connect(from, password);
+			transport.connect(authUser, password);
+			transport.sendMessage(msg, msg.getAllRecipients());
+
     		System.out.println("MailSample end.");
 
 		} catch (MessagingException mex) {
@@ -132,6 +147,19 @@ public class MailSample {
 	private static void usage() {
 		System.out.println(
 				"usage: java msgsendsample <to> <from> <password> <smtp> <port> true|false");
+	}
+
+	// SMTP
+	private static class SMTPAuthenticator extends Authenticator {
+		private String username;
+		private String password;
+		SMTPAuthenticator(String username, String password) {
+			this.username = username;
+			this.password = password;
+		}
+		public PasswordAuthentication getPasswordAuthentication() {
+			return new PasswordAuthentication(username, password);
+		}
 	}
 
 }
